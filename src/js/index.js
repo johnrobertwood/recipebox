73389f805c0c603774116e80f734a27068393ffb
John Wood <johnrobertwood@yahoo.com>
2016-05-22 15:28:56 -0700

    Creating add recipe feature

--- src/js/index.js
+++ src/js/index.js
@@ -26,8 +26,9 @@ var RecipeList = React.createClass({
 		var Accordion = ReactBootstrap.Accordion;
 		var Panel = ReactBootstrap.Panel;
 		var Button = ReactBootstrap.Button;
+		var index = 0;
 		this.props.recipes.forEach(function(recipe) {
-			rows.push(<Panel header={recipe.name}>
+			rows.push(<Panel header={recipe.name} eventKey={index++}>
 				<p>Ingredients:</p>
 				<IngredientList recipe={recipe} />
 				</Panel>
@@ -42,14 +43,107 @@ var RecipeList = React.createClass({
 });
 
 var RecipeTable = React.createClass({
+	getInitialState: function() {
+		return {recipes: this.props.recipes}
+	},
+	handleRecipeSubmit: function(recipe) {
+		//TODO: submit change and refresh the list
+		RECIPES.push(recipe);
+		this.setState({recipes: RECIPES});
+	},
 	render: function() {
 		return (
 			<div>
-			  <RecipeList recipes={this.props.recipes} />
-				<AddRecipeModal />
+			  <RecipeList recipes={this.state.recipes} />
+				<AddRecipeModal onRecipeSubmit={this.handleRecipeSubmit} />
 		  </div>
 		);
 	}
+});
+
+var AddRecipeModal = React.createClass({
+
+  getInitialState: function() {
+    return { 
+    	showModal: false,
+    	name: '',
+    	ingredients: ''
+    };
+  },
+
+  close: function() {
+    this.setState({ showModal: false });
+  },
+
+  open: function() {
+    this.setState({ showModal: true });
+  },
+
+  addRecipe: function() {
+  	alert("Add a recipe");
+  },
+
+  getValidationState: function() {
+    const length = this.state.name.length;
+    if (length > 2) return 'success';
+    else if (length > 0) return 'error';
+  },
+
+  handleNameChange: function(e) {
+    this.setState({ name: e.target.value });
+  },
+
+  handleIngredientChange: function(e) {
+		this.setState({ingredients: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+  	e.preventDefault();
+  	var name = this.state.name;
+  	var ingredients = this.state.ingredients.split(',')
+  	this.props.onRecipeSubmit({name: name, ingredients: ingredients});
+  	this.close();
+  	this.setState({name: '', ingredients: []});
+  },
+
+  render: function() {
+  	var Modal = ReactBootstrap.Modal;
+		var Button = ReactBootstrap.Button;
+		var FormGroup = ReactBootstrap.FormGroup;
+		var FormControl = ReactBootstrap.FormControl;
+		var ControlLabel = ReactBootstrap.ControlLabel;
+    return (
+      <div>
+      	<Button onClick={this.open} bsStyle="primary">Add Recipe</Button>
+
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Recipe</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+	          <form>
+	            <FormGroup controlId="formBasicText" validationState={this.getValidationState()}>
+	    	        <ControlLabel>Recipe</ControlLabel>
+	              <FormControl type="text"
+	                value={this.state.name}
+	                placeholder="Recipe"
+	                onChange={this.handleNameChange}/>
+	    	        <ControlLabel>Ingredients</ControlLabel>
+	              <FormControl type="text"
+	                value={this.state.ingredients}
+	                placeholder="Enter ingredients separated by commas"
+	                onChange={this.handleIngredientChange}/>
+	            </FormGroup>
+	          </form>
+          </Modal.Body>
+          <Modal.Footer>
+          	<Button onClick={this.handleSubmit} bsStyle="primary">Add Recipe</Button>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 });
 
 var EditRecipeModal = React.createClass({
@@ -136,81 +230,11 @@ var EditRecipeModal = React.createClass({
   }
 });
 
-var AddRecipeModal = React.createClass({
 
-  getInitialState: function() {
-    return { 
-    	showModal: false,
-    	name: '',
-    	ingredients: [] 
-    };
-  },
-
-  close: function() {
-    this.setState({ showModal: false });
-  },
-
-  open: function() {
-    this.setState({ showModal: true });
-  },
-
-  addRecipe: function() {
-  	alert("Add a recipe");
-  },
-
-  getValidationState: function() {
-    const length = this.state.name.length;
-    if (length > 2) return 'success';
-    else if (length > 0) return 'error';
-  },
-
-  handleChange: function(e) {
-    this.setState({ value: e.target.value });
-  },
-
-  render: function() {
-  	var Modal = ReactBootstrap.Modal;
-		var Button = ReactBootstrap.Button;
-		var FormGroup = ReactBootstrap.FormGroup;
-		var FormControl = ReactBootstrap.FormControl;
-		var ControlLabel = ReactBootstrap.ControlLabel;
-    return (
-      <div>
-      	<Button onClick={this.open} bsStyle="primary">Add Recipe</Button>
-
-        <Modal show={this.state.showModal} onHide={this.close}>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Recipe</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-	          <form>
-	            <FormGroup controlId="formBasicText" validationState={this.getValidationState()}>
-	    	        <ControlLabel>Recipe</ControlLabel>
-	              <FormControl type="text"
-	                value={this.state.name}
-	                placeholder="Recipe"
-	                onChange={this.handleChange}/>
-	    	        <ControlLabel>Ingredients</ControlLabel>
-	              <FormControl type="text"
-	                value={this.state.ingredients}
-	                placeholder="Enter ingredients separated by commas"
-	                onChange={this.handleChange}/>
-	            </FormGroup>
-	          </form>
-          </Modal.Body>
-          <Modal.Footer>
-          	<Button onClick={this.addRecipe} bsStyle="primary">Add Recipe</Button>
-            <Button onClick={this.close}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
-});
 
 var RECIPES = [
-	{name: "Peanut Butter Jelly", ingredients: ["Peanut Butter", "Jelly", "Bread"], index: "1"},
-	{name: "Burrito", ingredients: ["Beans", "Rice", "Tortilla", "Salsa"], index: "2"}
+	{name: "Peanut Butter Jelly", ingredients: ["Peanut Butter", "Jelly", "Bread"]},
+	{name: "Burrito", ingredients: ["Beans", "Rice", "Tortilla", "Salsa"]}
 ]
 
 ReactDOM.render(
